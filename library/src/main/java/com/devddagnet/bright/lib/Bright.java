ab014b75ec873006ab68a415cfa14975dd238c4b
DAGNET Damien <dagnet.damien@gmail.com>
2014-05-29 00:02:48 +0200

    added formula variants: relative & perceived. added performance boost. + config possibility. improved design

--- library/src/main/java/com/devddagnet/bright/lib/Bright.java
+++ library/src/main/java/com/devddagnet/bright/lib/Bright.java
@@ -1,51 +1,100 @@
 package com.devddagnet.bright.lib;
 
 import android.graphics.Bitmap;
-import android.graphics.Color;
 
 public class Bright {
 
-    public static boolean isBright(int luminance) {
-        return (luminance > 130);
+    private static final Luminance sInstance = new Luminance();
+
+    private Bright() {
+
     }
 
-    public static int getBrightness(int color) {
-        int r = (color >> 16) & 0xFF;
-        int g = (color >> 8) & 0xFF;
-        int b = color & 0xFF;
-
-        return (getBrightness(r, g, b));
+    public static Luminance getInstance() {
+        return sInstance;
     }
 
-    public static int getBrightness(int[] rgb) {
-        return (getBrightness(rgb[0], rgb[1], rgb[2]));
+    public static Luminance setup(int config) {
+        return new Luminance(config);
     }
 
-    public static int getBrightness(int r, int g, int b) {
-        return ((r + r + r + b + g + g + g + g) >> 3);
-    }
+    public static final class Luminance {
 
-    public static int[] averageRGB(Bitmap pic) {
-        int R, G, B;
-        int pixelColor;
-        int width = pic.getWidth();
-        int height = pic.getHeight();
-        int size = width * height;
+        private int mConfig;
 
-        R = G = B = 0;
-        for (int x = 0; x < width; ++x) {
-            for (int y = 0; y < height; ++y) {
-                pixelColor = pic.getPixel(x, y);
-                R += Color.red(pixelColor);
-                G += Color.green(pixelColor);
-                B += Color.blue(pixelColor);
-            }
+        private Luminance() {
+            this(Config.RELATIVE);
         }
 
-        R /= size;
-        G /= size;
-        B /= size;
+        private Luminance(int config) {
+            mConfig = config;
+        }
 
-        return (new int[]{ R, G, B });
+        public Luminance config(int config) {
+            mConfig = config;
+            return (this);
+        }
+
+        public boolean isBright(int luminance) {
+            return (luminance > 130);
+        }
+
+        public boolean isBright(Bitmap bitmap) {
+            return isBright(brightness(bitmap));
+        }
+
+        public boolean isBright(int[] rgb) {
+            return isBright(brightness(rgb));
+        }
+
+        public boolean isBright(int r, int g, int b) {
+            return isBright(brightness(r, g, b));
+        }
+
+        public int brightness(Bitmap bitmap) {
+            return this.brightness(Utility.getAverageColorRGB(bitmap));
+        }
+
+        public int brightness(int color) {
+            int r = (color >> 16) & 0xFF;
+            int g = (color >> 8) & 0xFF;
+            int b = color & 0xFF;
+
+            return (this.brightness(r, g, b));
+        }
+
+        public int brightness(int[] rgb) {
+            return (this.brightness(rgb[0], rgb[1], rgb[2]));
+        }
+
+        public int brightness(int r, int g, int b) {
+            switch (mConfig) {
+                case Config.RELATIVE | Config.PERFORMANCE:
+                    return ((r + r + b + g + g + g) / 6);
+                case Config.PERCEIVED | Config.PERFORMANCE:
+                    return ((r + r + r + b + g + g + g + g) >> 3);
+                case Config.RELATIVE:
+                    return (Math.round((0.2126f * r + 0.7152f * g + 0.0722f * b)));
+                case Config.PERCEIVED:
+                default:
+                    return (Math.round(0.299f * r + 0.587f * g + 0.114f * b));
+            }
+        }
+    }
+
+    public static final class Config {
+
+        /**
+         * trade accuracy for performance
+         */
+        public static final int PERFORMANCE = 1;
+        /**
+         * luminance relative: Luma CCIR 601
+         */
+        public static final int RELATIVE    = 2;
+        /**
+         * luminance perceived: Luma BT. 709
+         */
+        public static final int PERCEIVED   = 4;
     }
 }
---
+++ library/src/main/java/com/devddagnet/bright/lib/Utility.java
@@ -0,0 +1,31 @@
+package com.devddagnet.bright.lib;
+
+import android.graphics.Bitmap;
+import android.graphics.Color;
+
+public class Utility {
+
+    public static int[] getAverageColorRGB(Bitmap bitmap) {
+        int R, G, B;
+        int pixelColor;
+        int width = bitmap.getWidth();
+        int height = bitmap.getHeight();
+        int size = width * height;
+
+        R = G = B = 0;
+        for (int x = 0; x < width; ++x) {
+            for (int y = 0; y < height; ++y) {
+                pixelColor = bitmap.getPixel(x, y);
+                R += Color.red(pixelColor);
+                G += Color.green(pixelColor);
+                B += Color.blue(pixelColor);
+            }
+        }
+
+        R /= size;
+        G /= size;
+        B /= size;
+
+        return (new int[]{ R, G, B });
+    }
+}
